28246b32a6066ff870c9423615a6f50c954717b2
ggaleote <gabriel.galeotecheca@analog.com>
2022-04-21 10:47:46 +0900

    refactor project code

--- MATLAB_CODE/AlgoritmoDeteccionQRS.m
+++ MATLAB_CODE/AlgoritmoDeteccionQRS.m
@@ -1,103 +1,82 @@
-% Título: Algoritmo de Pam-Tompkinds de la señal
-% Autor: Gabriel Galeote Checa
-% DNI: 77231069J
-% -----------------------------------------------------
-% Limpieza del espacio de trabajo.
+% % Pam Tompkins implementation for ecg beat detection
+% Author: Gabriel Galeote Checa
+% Clean Workspace
 clear all
 close all
 
-% 1.- Cargar la señal del ECG de un fichero externo
+% 1 - Load ECG signal from external file
 
-% Código para cargar ficheros de la base de datos
-%ECG_1 = load('A00001m.mat'); % Cargamos el fichero con el ECG.
+% Load file from the data base
+%ECG_1 = load('A00001m.mat');
 %ECG_1 = ECG_1.val(1,:);
 
-% Código para cargar ficheros leídos por el sistema
 load ('ECG_exp_pac_4.txt');
 ECG_1=ECG_exp_pac_4;
 
-% Se invierte la señal para obtenerla en la forma adecuada si se necesita.
+% (optional) Invert signal in case the polarization of the electrodes is not the correct
 ECG_1 = ECG_1*(-1); 
 
-% Cálculo de la longitud de la secuencia
-longitud = length (ECG_1);
-muestras=longitud;
+% Calulate length of the signal
+samples = length (ECG_1);
 
-% Creación del intervalo de representación de la señal, en muestras
-m = 0:(longitud-1);
+% Representation interval for the signal in samples units
+m = 0:(samples-1);
 
-% Frecuencia de muestreo 100 para las propias, 500 para las bases de datos
+% Sampling Frequency
 Fs=54;
 
-% Resolución de la señal (periodo de muestreo)
+% Signal resolution
 Ts = 1 / Fs;
 
-% Intervalo de tiempo de representación
-t = 0:Ts:(longitud-1)/Fs;
+% representation time interval
+t = 0:Ts:(samples-1)/Fs;
 
-% Factor  de escalado del vector a representar de forma que represente
-% entre 0 y 35 Hz.
-escalado=Fs/35;
+% Scale factor of the vector from 0 to 35 Hz
+scaling=Fs/35;
 
-% 2 - Eliminar el OFFSET de esta señal del ECG.
+% 2 - Remove DC component (offset)
 
-% Cálculo de la señal de ECG sin el nivel de continua, para ajustarla 
-% en el eje Y.
 ECG_sindc=ECG_1-mean(ECG_1);
 
-% 3.- Primer análisis en frecuencia (FFT) de toda la señal del ECG
+% 3.- Compute FFT
 
-% Cálculo de FFT de la señal del ECG sin DC y generación de un vector 
-% con las muestras en frecuencia para el eje X de la representación
+% Compute FFT of the ECG signal without DC
+senal_fft = fft (ECG_sindc,samples);
 
-% Obtención de la señal fft (transformada rápida de fourier)
-senal_fft = fft (ECG_sindc,longitud);
+% Magnitude of the FFT
+magnitude = abs (senal_fft);
 
-% cálculo de la magnitud de la señal fft
-Magnitud = abs (senal_fft);
-
-% Cálculo del eje X
-EjeX = linspace (0,Fs-(Fs/longitud),longitud);
+% Calculate X-axis
+x_axis = linspace (0,Fs-(Fs/samples),samples);
 
 % Figura 1, subplot 2
-% Representación gráfica de la magnitud en frecuencia del ECG sin DC
 figure(1); subplot(2,1,1);
 plot (t, ECG_sindc); grid on;
-title('Representación de ECG sin nivel de continua')
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+title('ECG without DC')
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
 figure (1); subplot(2,1,2);
-plot (EjeX(1:(longitud/2)+1), Magnitud(1:(longitud/2)+1)); grid;
-title ('Representación en Frecuencia del ECG sin nivel de continua');
-xlabel ('Frecuencia (Hz)');ylabel('|Magnitud| (mV)');
+plot (x_axis(1:(samples/2)+1), magnitude(1:(samples/2)+1)); grid;
+title ('FFT of the ECG without DC');
+xlabel ('Frequency (Hz)');ylabel('|Magnitude| (mV)');
 
 
-% 4 - Representar la señal del ECG sin OFFTSET y FFT entre 0 y 35 Hz
-
-% Representación gráfica del ECG sin DC de nuevo en tiempo y en
-% frecuencia.
+% 4 - Represent signal from 0 to 35 Hz
 
 figure(2); subplot(2,1,1);
 plot (t, ECG_sindc); grid on;
-title('Representación de ECG sin nivel de continua')
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+title('ECG without DC')
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
 figure(2); subplot(2,1,2);
-plot(EjeX(1:(longitud/escalado)+1),Magnitud(1:(longitud/escalado)+1));
+plot(x_axis(1:(samples/scaling)+1),magnitude(1:(samples/scaling)+1));
 grid on;
-title ('Representación en Frecuencia del ECG sin nivel de continua');
-xlabel ('Frecuencia (Hz)');ylabel('|Magnitud| (mV)');
-
-% Se muestra a parte para que se vea en una escala mayor la señal 
-% sin nivel de continua
-figure(21);
-plot (t, ECG_sindc); grid on;
-title('Representación de ECG sin nivel de continua')
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+title ('FFT of the signal without DC');
+xlabel ('Frquency (Hz)');ylabel('|Magnitude| (mV)');
 
 % ---------------------------------------------
 
-% Filtro pasa baja
+% Low Pass Filter
 
 Fpass = 14;          % Passband Frequency
 Fstop = 20;          % Stopband Frequency
@@ -110,7 +89,7 @@ h2  = fdesign.lowpass(Fpass, Fstop, Apass, Astop, Fs);
 H2 = design(h2, 'butter', 'MatchExactly', match);
 
 
-% Filtro pasa alta 
+% High Pass filter 
 
 Fstop = 1;           % Stopband Frequency
 Fpass = 6;           % Passband Frequency
@@ -122,163 +101,133 @@ match = 'passband';  % Band to match exactly
 h1  = fdesign.highpass(Fstop, Fpass, Astop, Apass, Fs);
 H1 = design(h1, 'cheby2', 'MatchExactly', match);
 
-% Filtrado de la señal
-ECG_filtrado1 = filter (H1,ECG_sindc);
-ECG_filtrado2 = filter (H2,ECG_filtrado1);
+% Filter
+ecg_filtered_1 = filter (H1,ECG_sindc);
+ecg_filtered_2 = filter (H2,ecg_filtered_1);
 
-% Dibujo la señal ECG_filtrado3 en función del tiempo y de la frecuencia
-ECGFFT1 = fft(ECG_filtrado1 ,longitud);
-ECGFFT2 = fft(ECG_filtrado2 ,longitud);
+ECGFFT1 = fft(ecg_filtered_1 ,samples);
+ECGFFT2 = fft(ecg_filtered_2 ,samples);
 
-% Cálculo del módulo de la señal
-Magnitud1 = abs(ECGFFT1);
-Magnitud2 = abs(ECGFFT2);
+magnitude1 = abs(ECGFFT1);
+magnitude2 = abs(ECGFFT2);
 
-% Calculo de las transformadas inversas de fourier
-ECGIFFT1 = ifft(ECG_filtrado1);
-ECGIFFT2 = ifft(ECG_filtrado2);
+% Inverse fourier transform
+ECGIFFT1 = ifft(ecg_filtered_1);
+ECGIFFT2 = ifft(ecg_filtered_2);
 
 % ----------------------------
 figure(3); subplot(2,1,1);
-plot(t,ECG_filtrado1); grid on;
-title('Representación de ECG filtrado por pasa-alta');
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+plot(t,ecg_filtered_1); grid on;
+title('High pass filter of the ECG');
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
 figure(3); subplot(2,1,2);
-plot(EjeX(1:(longitud/2)+1), Magnitud1(1:(longitud/2)+1)); grid on;
-title ('Magnitud ECG filtrado por filtro pasa-alta');
-xlabel ('Frecuencia (Hz)');ylabel ('|Magnitud|');
+plot(x_axis(1:(samples/2)+1), magnitude1(1:(samples/2)+1)); grid on;
+title('High pass filtered FFT of the ECG');
+xlabel ('Frequency (Hz)');ylabel ('|Magnitude|');
 
 % -----------------------------
 figure(4); subplot(2,1,1);
-plot(t,ECG_filtrado2); grid on;
-title('Representación de ECG filtrado por pasa-baja');
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+plot(t,ecg_filtered_2); grid on;
+title('Low pass filter of the ECG');
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
 figure(4); subplot(2,1,2);
-plot(EjeX(1:(longitud/2)+1), Magnitud2(1:(longitud/2)+1)); grid on;
-title ('Magnitud ECG filtrado por filtro pasa-baja');
-xlabel ('Frecuencia (Hz)');ylabel ('|Magnitud|');
+plot(x_axis(1:(samples/2)+1), magnitude2(1:(samples/2)+1)); grid on;
+title('Low pass filtered FFT of the ECG');
+xlabel ('Frequency (Hz)');ylabel ('|Magnitude|');
 
-% -----------------------------------------------
-% 5 - Calcular la derivada de la señal
-% -----------------------------------------------
-w = 0:2*pi/longitud:2*pi-(2*pi/longitud);
+% 5 - Derivative of the signal
 
-% Definimos el filtro que calcula la derivada del ECG filtrado
+w = 0:2*pi/samples:2*pi-(2*pi/samples);
+
 H3 = (5.0 + 4.0.*exp(-1i*w) + 3.0.*exp(-1i*w*2) + 2.0.*exp(-1i*w*3)...
     + exp(-1i*w*4)- 1.0.*exp(-1i*w*6) - 2.0.*exp(-1i*w*7) - ...
     3.0.*exp(-1i*w*8) - 4.0.*exp(-1i*w*9) - 5.0.*exp(-1i*w*10)) / 110.0;
 
-% Normalización del filtro
+% Normalization of the filter
 H3_norm = H3./max(abs(H3));
 
-% Multiplicamos en frecuencia dato a dato la señal ya filtrada
+% Multiplate sample by sample in frequency
 ECGFFT3 = ECGFFT2.*H3_norm;
 
-% Calculamos FFT inversa.
-ECGIFFT3 = ifft(ECGFFT3,muestras);
+ECGIFFT3 = ifft(ECGFFT3,samples);
 
-% Figura 5
-% Representación gráfica la señal derivada en frecuencia y en tiempo
-figure (5); subplot (2,1,2);
-plot(EjeX(1:muestras/escalado),abs(ECGFFT3(1:muestras/escalado)));
-grid on;
-title('ECG en frecuencia filtado por el filtro H5 (derivada)');
-xlabel ('Frecuencia (Hz)'); ylabel ('|Magnitud| (mV)');
-
+% Figure 5
 figure (5); subplot (2,1,1); 
 plot (t,real(ECGIFFT3)); grid on;
-title('ECG filtrado en tiempo por el filtro H5 (derivada)');
-xlabel ('Tiempo (s)'); ylabel ('Amplitud (mV)');
+title('ECG filtered by filter H5 (derivative)');
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
-% ---------------------------------------------------------------
-% 6 - Elevar la señal al cuadrado muestra a muestra, en el tiempo
-%
-% En este apartado se eleva la señal al cuadrado
-% ---------------------------------------------------------------
+figure (5); subplot (2,1,2);
+plot(x_axis(1:samples/scaling),abs(ECGFFT3(1:samples/scaling)));
+grid on;
+title('ECG FFT filtered by the filter  H5 (derivative)');
+xlabel ('Frequency (Hz)'); ylabel ('|Magnitude| (mV)');
 
-ECGIFFT4 = ECGIFFT3.^2; % Elevamos aquí la señal al cuadrado
+% 6 - Square the signal sample by sample
 
-% Figura 6, Representación de la señal elevada al cuadrado.
+ECGIFFT4 = ECGIFFT3.^2; 
+
+% Figure 6
 figure(6);
 subplot (2,1,1); plot (t,real(ECGIFFT4)); grid on;
-title('ECG filtrado en tiempo por el filtro H5 y elevado al cuadrado');
-xlabel ('Tiempo (s)');
-ylabel ('Amplitud (mV)');
+title('ECG squared and filtered by H5');
+xlabel ('Time (s)');
+ylabel ('Amplitude (mV)');
 
-% --------------------------------------------
-% 9.- Aplicar una ventana de integración a la señal
-%
-% En este apartado se integra la señal dentro de una ventana 
-% de integración para obtener los complejos QRS
-% --------------------------------------------
+% 9 - Signal integration window 
 
-%Tiempo de ventana, por defecto 150 ms
-tiempo_ventana = 0.150;
+% Window time, by default 150 ms
+window = 0.150;
 
-% Nº de muestras de la ventana de integración
-muestras_vent = ceil(Fs*tiempo_ventana);
+% window number of samples
+samples_window = ceil(Fs*window);
 
-% Aplicar la ventana de integración.
 a = 1;
-c=zeros(1,muestras_vent);
-b = c+(1/muestras_vent);
+c=zeros(1,samples_window);
+b = c+(1/samples_window);
 ECG_integ = filter (b,a,ECGIFFT4);
 
 % Figura 6, subplot 2
-% Representamos gráficamente la señal en el tiempo
 figure (6); subplot (2,1,2);
 plot (t,real(ECG_integ));grid on;
-title ('ECG filrado por el filtro H5, elevado al cuadrado e integrado');
-xlabel('Tiempo(s)'); ylabel('Amplitud (mV)');
+title ('ECG filtered by H5, squared and integrated');
+xlabel('Time (s)'); ylabel('Amplitude (mV)');
 
-% -----------------------------------------------------
-% 10.- Algoritmo de detección del QRS
-% -----------------------------------------------------
-% Inicializamos los valores de "spki" y "npki"
+% 10 - QRS detection
 
-% Tiempo de entrenamiento
+% Training time
 entren = 1;
 
 ECG_integ = real(ECG_integ);
 
-% spki será 1/3 del máximo de la señal integrada en ese periodo
+% spki will be 1/3 from the maximum of the signal integrated in the training time
 spki = max (ECG_integ(1:entren*Fs)) / 3;
 
-% npki será la mitad de la media de la señal integrada en ese periodo
+% npki will be the half of the mean on the integrated signal
 npki = mean (ECG_integ(1:entren*Fs)) / 2;
 
-% Inicialización el umbral "thri1" que nos indicará si el pico
-% es de ruido (<= thri1) o señal (> thri1)
+% Initialize the threshold "thri1" that will check if the peak is noise (<= thri1) or signal (> thri1)
 thri1 = npki +0.25*(spki-npki);
 
-% Buscamos los picos en la señal integrada (utilizar "findpeaks")
+% Find peaks of the integrated signal
 [pks,loc] = findpeaks(abs(ECG_integ));
 
-% Creación de un vector relleno de ceros que indicará si hay QRS en un
-% punto o no. Si hay 0, no hay QRS, si hay otro valor, sí lo hay.
-QRS = zeros(1,muestras);
+QRS = zeros(1,samples);
 
-% Vector para llevar el control de los "thri1", "npkis" y 
-% "spkis" que hay. En la evaluación de cada pico encontrado.
-thrs = zeros(1,muestras);
-spks = zeros(1,muestras);
-npks = zeros(1,muestras);
+thrs = zeros(1,samples);
+spks = zeros(1,samples);
+npks = zeros(1,samples);
 
-%bucle para identificar si cada pico es de señal o ruido.
+% loop
 for i = 1:length(pks)
-    % Si el pico es mayor que threshold lo consideramos señaly 
-    % se actualiza el valor de spki y de threshold para el 
-    % estudio del siguiente pico.
-    if(pks(i)>thri1)
-        spki = 0.125*pks(i)+0.875*spki;
-        % El valor de maximo será en función del máximo del ECG.
+    if(pks(i)>thri1) % is a peak
+        spki = 0.125*pks(i)+0.875*spki;  % update spki
         QRS(loc(i))= 0.5*max(abs(ECG_integ));
         
     else
-        %En caso contrario, el pico se considerará ruido y actualiza
-        % npki y a continuación del threshold.
+        % noise, then update npki
         npki = 0.125*pks(i)+0.875*npki;
     end
     thri1 = npki+0.25*(spki-npki);
@@ -287,8 +236,7 @@ for i = 1:length(pks)
     npks(loc(i)) = npki;
 end;
 
-% Figura 7, subplot 1
-% Dibujamos la ECG integrada y el vector QRS en dos subplots
+% Figure 7, subplot 1
 QRS_positivos = find(QRS > 0);
 
 figure (7); subplot(2,1,1);
@@ -305,38 +253,32 @@ plot(t(spks_positivos),spks(spks_positivos), 'g+');hold on;
 npks_positivos = find(npks > 0);
 plot(t(npks_positivos),npks(npks_positivos), 'k.'); hold on;
 
-legend('SEÑAL','QRS','THRESHOLD I1','SPKI','NPKI');
-title('ECG integrado + thri1 + spki + npki + QRS original');
-xlabel('Tiempo(s)'); ylabel('Amplitud (mV)');
+legend('Signal','QRS','THRESHOLD I1','SPKI','NPKI');
+title('ECG integrated + thri1 + spki + npki + QRS original');
+xlabel('Time (s)'); ylabel('Amplitude (mV)');
 
 % Figura 7, subplot 2
 % Dibujamos la ECG sin DC para comprobar los sitios donde hay 
 % complejos QRS
 figure (7); subplot(2,1,2); plot(t,ECG_1);grid on;
-title('ECG sin DC');
-xlabel('Tiempo(s)'); ylabel('Amplitud');
+title('ECG no DC');
+xlabel('Time (s)'); ylabel('Amplitude (mV)');
 
-%----------------------------------------------------
-% Mejora del algoritmo
-% ---------------------------------------------------
+% Algorithm improvement
 
-muestras_entre_picos = Fs*0.2;
+samples_between_peaks = Fs*0.2;
 
-% En la función findpeaks se especifica que la distancia entre 
-% picos debe ser del 20% de la frecuencia de muestreo. Es un valor 
-% que se puede modificar para ir afinando el algoritmo.
-[pks2,loc2] = findpeaks(ECG_integ,'MINPEAKDISTANCE',muestras_entre_picos);
+[pks2,loc2] = findpeaks(ECG_integ,'MINPEAKDISTANCE',samples_between_peaks);
 
-maximos = zeros(1,muestras);
-thrs_2 = zeros(1,muestras);
-spks_2 = zeros(1,muestras);
-npks_2 = zeros(1,muestras);
+maxs = zeros(1,samples);
+thrs_2 = zeros(1,samples);
+spks_2 = zeros(1,samples);
+npks_2 = zeros(1,samples);
 
 for i = 1:length(pks2)
     if(pks2(i)>thri1)
         spki = 0.125*pks2(i)+0.875*spki;
-        % El valor máximo será en función del máximo del ECG.
-        maximos(loc2(i)) = 0.5*max(abs(ECG_integ));
+        maxs(loc2(i)) = 0.5*max(abs(ECG_integ));
     else
         npki = 0.125*pks2(i)+0.875*npki;
     end
@@ -345,47 +287,46 @@ for i = 1:length(pks2)
     spks_2(loc2(i)) = spki;
     npks_2(loc2(i)) = npki;
 end
-% Figura 8, subplot 1
-% Dibujamos la ECG integrada, el vector QRS, el vector THRI1, 
-% el vector SPKI y el vector NPKI en un subplot mediante hold on
-maxs_posi=find(maximos > 0);
+% Figure 8, subplot 1
+% Plot integrated signal, the vector QRS, the vector THRI1, 
+% the vector SPKI and the vector NPKI in a subplot by a hold on
+maxs_posi=find(maxs > 0);
 
 figure (8); subplot(2,1,1);
 plot (t,ECG_integ); grid on; hold on;
-plot(t(maxs_posi),maximos(maxs_posi),'m^'); hold on;
+plot(t(maxs_posi),maxs(maxs_posi),'m^'); hold on;
 
-thrs_positivos_2 = find(thrs_2 > 0);
-plot(t(thrs_positivos_2), thrs_2(thrs_positivos_2), '*r');hold on;
+thrs_positives_2 = find(thrs_2 > 0);
+plot(t(thrs_positives_2), thrs_2(thrs_positives_2), '*r');hold on;
 
-spks_positivos = find(spks_2 > 0);
-plot(t(spks_positivos),spks_2(spks_positivos), 'g+');hold on;
+spks_positives = find(spks_2 > 0);
+plot(t(spks_positives),spks_2(spks_positives), 'g+');hold on;
 
-npks_positivos = find(npks_2 > 0);
-plot(t(npks_positivos),npks_2(npks_positivos), 'k.'); hold on;
+npks_positives = find(npks_2 > 0);
+plot(t(npks_positives),npks_2(npks_positives), 'k.'); hold on;
 
-legend('SEÑAL','QRS','THRESHOLD I1','SPKI','NPKI');
-title('ECG integrado + thri1 + spki + npki + QRS en la primera mejora');
-xlabel('Tiempo(s)');
-ylabel('Amplitud');
+legend('SIGNAL','QRS','THRESHOLD I1','SPKI','NPKI');
+title('ECG integrated + thri1 + spki + npki + QRS in the first improvement');
+xlabel('Time (s)');
+ylabel('Amplitude (mV)');
 
-% Figura 8, subplot 2
-% Representación gráfica de la ECG sin DC para comprobar los sitios 
-% donde se han encontrado complejos QRS
+% Figure 8, subplot 2
+% Graphic representation of the ECG without DC 
 
 figure (8);subplot(2,1,2);
 plot (t,ECG_1); grid on;
-title('ECG sin DC');
-xlabel('Tiempo(s)'); ylabel('Amplitud');
+title('ECG no DC');
+xlabel('Time (s)'); ylabel('Amplitude (mV)');
 
 % --------------------------------------
-%          Apartado estadístico
+%          Statistics
 % --------------------------------------
 
-QRSdetectado = findpeaks(maximos);
+QRSdetected = findpeaks(maxs);
 
-ntotalQRS = length(QRSdetectado);
+ntotalQRS = length(QRSdetected);
 
-tiempo=(longitud/Fs); % En segundos
-tiempominutos = tiempo / 60; % en minutos
+time = (samples/Fs); % in seconds
+time_minutes = time / 60; % in minutes
 
-frecuenciacardiaca = ntotalQRS/(tiempominutos);
+cardiac_frequency = ntotalQRS/(time_minutes);
--- MATLAB_CODE/ProcesadoFiltrado.m
+++ MATLAB_CODE/ProcesadoFiltrado.m
@@ -1,67 +1,64 @@
 %----------------------------------------------------------------------
-% Título: Código de Procesamiento de datos mediante MATLAB
-% Autor: Gabriel Galeote Checa
-% Fecha: XX/XX/XXXX
-% En este código se realiza el desarrollo de la parte de proesamiento de
-% datos obtenidos a partir de una lectura del electrocardiógrafo.
+% Title: Signal Processing Code in Matlab
+% Author: Gabriel Galeote Checa
+% Last update: 21/04/2022
+% This code is a signal processing code for ecg 
 %----------------------------------------------------------------------
 clear all;
 close all;
 
-% 1.- Señal de ECG
-% En este apartado se va a cargar el ECG "e1071230.MAT", eliminar la componente de continua,
-% y representarlo gráficamente en función del tiempo y de la frecuencia.
+% 1 - Load ecg signal
+% Load "e1071230.MAT", remove dc component and graphic representation
 
-% load('100m.mat'); % Cargamos el fichero con el ECG.
- %ECG_1 = load('100m.mat'); % Cargamos el fichero con el ECG.
+% load('100m.mat');
+ %ECG_1 = load('100m.mat');
  %ECG_1 = ECG_1.val(1,:);
 
 load ('ECG_exp_pac_1.txt');
 ECG_1=ECG_exp_pac_1;
 
 
-%ECG_1 = 100m;% Definimos ECG_1 como la variable de entrada con el ECG.
+%ECG_1 = 100m;
 
-% Cálculo de la longitud de la secuencia
-longitud = length (ECG_1);
+% Calculate the length of the sequence
+n_samples = length (ECG_1);
 
-% frec. de muestreo
+% Sampling frequency
 Fs = 360; 
 
-% Periodo de muestreo
+% Sampling period
 Ts = 1 / Fs; 
 
-% Creamos el intervalo de representación de la señal, en muestras
-t = 0:Ts:(longitud-1)/Fs;
+% Representation number of points
+t = 0:Ts:(n_samples-1)/Fs;
 
-% A continuación, se elimina la componente de continua.
-ECG_sin_DC = ECG_1 - mean (ECG_1);
+% remove DC
+ECG_no_DC = ECG_1 - mean (ECG_1);
 
-% Número de muestras de la señal
-longitud = length (ECG_sin_DC);
+% number of samples
+n_samples = length (ECG_no_DC);
 
-% obtención de la señal fft
-senal_fft = fft (ECG_sin_DC,longitud); 
+signal_fft = fft (ECG_no_DC,n_samples); 
 
-% cálculo de la magnitud a parte de la señal fft
-Magnitud = abs (senal_fft); 
+% calculate magnitude of the fft
+magnitude = abs (signal_fft); 
 
-%Cálculo del eje X para la representación en frecuencia
-EjeX = linspace (0,Fs-(Fs/longitud),longitud); 
+% X axis calculation
+x_axis = linspace (0,Fs-(Fs/n_samples),n_samples); 
 
 figure(1); subplot(2,1,1); 
-plot (t, ECG_sin_DC); grid on;
-title('Representación de ECG sin nivel de continua')
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+plot (t, ECG_no_DC); grid on;
+title('Representation of the ECG without DC')
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
 figure(1); subplot(2,1,2);
-plot (EjeX(1:(longitud/2)+1), Magnitud(1:(longitud/2)+1)); grid on;
-title ('Representación en Frecuencia del ECG sin nivel de continua');
-xlabel ('Frecuencia (Hz)');ylabel('|Magnitud|');
+plot (x_axis(1:(n_samples/2)+1), magnitude(1:(n_samples/2)+1)); grid on;
+title ('FFT representation of the signal without DC');
+xlabel ('Frequency (Hz)');ylabel('|Magnitude|');
 
 % ---------------------------------------------
 
-% Filtro pasa baja
+% Low pass filter
 
 Fpass = 21;          % Passband Frequency
 Fstop = 31;          % Stopband Frequency
@@ -73,7 +70,7 @@ match = 'passband';  % Band to match exactly
 h1  = fdesign.lowpass(Fpass, Fstop, Apass, Astop, Fs);
 H1 = design(h1, 'cheby2', 'MatchExactly', match);
 
-% Filtro pasa alta 
+% High Pass filter 
 
 Fstop = 1;           % Stopband Frequency
 Fpass = 9;           % Passband Frequency
@@ -85,38 +82,34 @@ match = 'passband';  % Band to match exactly
 h2  = fdesign.highpass(Fstop, Fpass, Astop, Apass, Fs);
 H2 = design(h2, 'cheby2', 'MatchExactly', match);
 
-% Filtrado de la señal
-ECG_filtrado1 = filter (H1,ECG_sin_DC);
+% Signal Filtering
+ECG_filtrado1 = filter (H1,ECG_no_DC);
 ECG_filtrado2 = filter (H2,ECG_filtrado1);
 
-% Dibujo la señal ECG_filtrado3 en función del tiempo y de la frecuencia
-senal_fft_1 = fft(ECG_filtrado1 ,longitud);
-senal_fft_2 = fft(ECG_filtrado2 ,longitud);
+signal_fft_1 = fft(ECG_filtrado1 ,n_samples);
+signal_fft_2 = fft(ECG_filtrado2 ,n_samples);
 
-% Cálculo del módulo de la señal
-Magnitud1 = abs(senal_fft_1);
-Magnitud2 = abs(senal_fft_2);
+% Calculate abs of the signal
+magnitude1 = abs(signal_fft_1);
+magnitude2 = abs(signal_fft_2);
 
-% ----------------------------
 figure(2); subplot(2,1,1);
 plot(t,ECG_filtrado1); grid on;
-title('Representación de ECG filtrado por pasa-baja');
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+title('ECG filtered with the low pass');
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
 figure(2); subplot(2,1,2);
-plot(EjeX(1:(longitud/8)+1), Magnitud1(1:(longitud/8)+1)); grid on;
-title ('Magnitud ECG filtrado');
-xlabel ('Frecuencia (Hz)');ylabel ('|Magnitud|');
+plot(x_axis(1:(n_samples/8)+1), magnitude1(1:(n_samples/8)+1)); grid on;
+title ('magnitude ECG filtered');
+xlabel ('Frequency (Hz)');ylabel ('|magnitude|');
 
 % -----------------------------
 figure(3); subplot(2,1,1);
 plot(t,ECG_filtrado2); grid on;
-title('Representación de ECG filtrado por pasa-alta');
-xlabel ('Tiempo (s)'); ylabel ('Tensión (mV)');
+title('ECG filtered by high pass');
+xlabel ('Time (s)'); ylabel ('Amplitude (mV)');
 
 figure(3); subplot(2,1,2);
-plot(EjeX(1:(longitud/8)+1), Magnitud2(1:(longitud/8)+1)); grid on;
-title ('Magnitud ECG filtrado');
-xlabel ('Frecuencia (Hz)');ylabel ('|Magnitud|');
-
-
+plot(x_axis(1:(n_samples/8)+1), magnitude2(1:(n_samples/8)+1)); grid on;
+title ('magnitude ECG filtrado');
+xlabel ('Frequency (Hz)');ylabel ('|magnitude|');
